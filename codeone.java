ee316727c1420caaa1ec19de6db1b572c188d64a
mrmanish227-ctrl <mrmanish227@gmail.com>
2026-01-29 09:02:09 +0530

    Add files via upload

---
+++ codeone.java
@@ -0,0 +1,17 @@
+package day2;
+
+public class codeone {
+public static void main(String[] args) {
+	int x;
+	int y;
+	double v;
+	short s;
+	long l;
+	char c;
+	boolean bool;
+	System.out.println(x);
+	System.out.println(y);
+	System.out.println(v);
+	
+}
+}
